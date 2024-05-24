d9cdeec68b8491c56576320e6dd79b6f2124e41f
TomasBeker <106827235+TomasBeker@users.noreply.github.com>
2024-05-24 19:41:27 -0300

    Add files via upload
    primer commit

---
+++ calculadora/funciones_matematicas.py
@@ -0,0 +1,122 @@
+def menu_calculadora()->str:
+    """Menu principal del programa.
+
+    Returns:
+        str: retorna la opcion elegida.
+    """    
+    print(f"{'Menu de opciones':^50s}")
+    print("1- Ingresar primer operando")
+    print("2- Ingresar segundo operando")
+    print("3- Calcular todas las operaciones")
+    print("4- Informar resultados")
+    print("5- Salir")
+    
+    return input("Ingrese una opcion: ")
+
+def sub_menu_calculo()->str:
+    """Menu de opciones de calculo.
+
+    Returns:
+        str: retorna la opcion elegida.
+    """    
+
+    print(f"{'Calculos disponibles':50s}")
+    print("A- Calcular la suma (A + B )")
+    print("B- Calcular la resta (A - B)")
+    print("C- Calcular la división(A / B)")
+    print("D- Calcular la multiplicación(A * B)")
+    print("E- Calcular factorial(A!)")
+
+    entrada = input("Ingrese una opcion: ")
+
+    return entrada.upper()
+
+def sub_menu_informar(primer_operando:float, segundo_operando:float)->str:
+    """Menu de opciones para informar el resultado de los calculos previamente hechos.
+
+    Args:
+        primer_operando (float): numero a calcular.
+        segundo_operando (float): numero a calcular.
+
+    Returns:
+        str: Retorna la opcion elegida.
+    """    
+
+    print(f"{'Resultados de los Calculos ':50s}")    
+    print(f"A- Resultado de la suma")
+    print(f"B- Resultado de la resta")
+    print(f"C- Resultado de la division")
+    print(f"D- Resultado de la multiplicacion")
+    print(f"E- Resultado del factorial de {primer_operando} y el de {segundo_operando}")
+
+    entrada = input("Ingrese una opcion: ")
+
+    return entrada.upper()
+
+def sumar(primer_operando:float, segundo_operando:float)-> float:
+    """Funcion hecha para sumar 2 numeros
+
+    Args:
+        primer_operando (float): numero a sumar.
+        segundo_operando (float): numero a sumar.
+
+    Returns:
+        float: retorna la suma
+    """    
+
+    return primer_operando + segundo_operando
+
+def restar(primer_operando:float, segundo_operando:float)-> float:
+    """Funcion hecha para restar 2 numeros
+
+    Args:
+        primer_operando (float): numero a restar.
+        segundo_operando (float): numero a restar.
+
+    Returns:
+        float: retorna la resta
+    """
+    return primer_operando - segundo_operando
+
+def dividir(primer_operando:float, segundo_operando:float)-> float:
+    """Funcion hecha para dividir 2 numeros
+
+    Args:
+        primer_operando (float): numero dividendo.
+        segundo_operando (float): numero divisor.
+
+    Returns:
+        float: retorna la division 
+    """
+    return primer_operando / segundo_operando
+
+def multiplicar(primer_operando:float, segundo_operando:float)-> float:
+    """Funcion hecha para multiplicar 2 numeros
+
+    Args:
+        primer_operando (float): numero a multiplicar.
+        segundo_operando (float): numero a multiplicar.
+
+    Returns:
+        float: retorna la multiplicacion
+    """
+    return primer_operando * segundo_operando
+
+def factorial(numero:int)->int:
+    """Funcion que busca el Factorial de un numero.
+
+    Args:
+        numero (int): Numero del cual se busca su factorial.
+
+    Returns:
+        int: Retorna el factorial.
+    """    
+    fact = None
+    if numero == 0 or numero == 1:
+        fact = 1
+    else:
+        fact = numero * factorial(numero - 1)
+
+    return fact
+
+    
---
+++ calculadora/main.py
@@ -0,0 +1,103 @@
+from funciones_matematicas import *
+import os
+
+seguir = "s"
+
+flag_primer_operando = False
+flag_segundo_operando = False
+
+flag_calculos = False
+
+
+
+while(seguir == "s"):
+    os.system("cls")
+
+    match menu_calculadora():
+        case "1":
+            a = int(input("Ingrese el primer operando: "))
+            flag_primer_operando = True
+        case "2":
+            b = int(input("Ingrese el segundo operando: "))
+            flag_segundo_operando = True
+        case "3":
+            if flag_primer_operando and flag_segundo_operando:
+                os.system("cls")
+
+                suma = sumar(a, b)
+
+                resta = restar(a, b)
+
+                division = dividir(a, b)
+
+                multiplicación = multiplicar(a, b)
+
+                factorial_a = factorial(a)
+                
+                factorial_b = factorial(b)
+
+                if a == 0 or b == 0:
+                    division = print("No es posible dividir por cero")
+                else:
+                    division = dividir(a, b)   
+
+                flag_calculos = True
+                print("Las operaciones han sido calculadas...") 
+            elif flag_primer_operando == False:
+                print("Para calcular primero debes ingresar el primer operando.")
+            else:
+                print("Para calcular primero debes ingresar el segundo operando.")
+        case "4":
+            if flag_primer_operando and flag_segundo_operando:
+                if flag_calculos:
+                    os.system("cls")
+                    match sub_menu_informar(a,b):
+                    
+                        case "A":
+                            
+                            print(f"El resultado de la suma entre {a} y {b} es: {suma}")
+                        case "B":
+                            print(f"El resultado de la resta entre {a} y {b} es: {resta}")
+                        case "C":
+                            print(f"El resultado de la division entre {a} y {b} es: {division}")
+                        case "D":
+                            print(f"El resultado de la multiplicacion entre {a} y {b} es: {multiplicación}")
+                        case "E":
+                            print(f"El resultado del factorial de {a} es:{factorial_a} y el de {b} es:{factorial_b} ")
+                else:
+                    print("Para informar los datos primero deben ser calculados")
+            elif flag_primer_operando == False:
+                print("Para calcular primero debes ingresar el primer operando.")
+            else:
+                print("Para calcular primero debes ingresar el segundo operando.")
+        case "5":
+            seguir = input("desea continuar?(s/para continuar n/para salir del programa): ")
+    
+    os.system("pause")
+
+
+
+
+    
+# match sub_menu_calculo():
+                    
+#                     case "A":
+#                         suma = sumar(a, b)
+#                         flag_suma = True
+#                     case "B":
+#                         resta = restar(a, b)
+#                         flag_resta = True
+#                     case "C":
+#                         if a == 0 or b == 0:
+#                             division = print("No es posible dividir por cero")
+#                             flag_division = True
+#                         else:
+#                             division = dividir(a, b)
+#                             flag_division = True
+#                     case "D":
+#                         multiplicación = multiplicar(a, b)
+#                         flag_multiplicacion = True    
+#                     case "E":
+#                         factorial_a = factorial(a)
+#                         factorial_b = factorial(b)
+#                         flag_factorial = True
